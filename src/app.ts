adf59de67b807e990b4075ca4c3a84bc877cf1e7
Geksanit <geksanit.w@gmail.com>
2020-12-21 18:36:57 +0700

    fix ts imports

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import { PAGINATION_PAGE_SIZE } from "./constants";
+
 const http = require("http");
 
 const hostname = "127.0.0.1";
 const port = 3000;
-console.log(module);
+console.log(PAGINATION_PAGE_SIZE, module);
 
 const server = http.createServer((req: any, res: any) => {
   res.statusCode = 200;
