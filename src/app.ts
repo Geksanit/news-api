1cf4a0f8b39e9c4568ad99c0469ea9282e27b9e1
Geksanit <geksanit.w@gmail.com>
2020-12-21 20:05:27 +0700

    add watch scripts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
-import { PAGINATION_PAGE_SIZE } from "./constants";
+import http from 'http';
 
-const http = require("http");
+import { PAGINATION_PAGE_SIZE } from './constants';
+import { HttpError } from './utils/Errors';
 
-const hostname = "127.0.0.1";
+const hostname = '127.0.0.1';
 const port = 3000;
-console.log(PAGINATION_PAGE_SIZE, module);
+console.log(PAGINATION_PAGE_SIZE, 1234);
 
-const server = http.createServer((req: any, res: any) => {
+const err = new HttpError(404, 'not found');
+console.log(err);
+
+const server = http.createServer((req, res) => {
+  console.log('request', req);
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
